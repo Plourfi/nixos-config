4e07af0a818cdf6c7389155c7f8ed1fe4bccb588
Plourfi <timocandi@gmail.com>
2025-09-14 13:47:23 +0200

    [No workie] Temp changes to try the patch

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,6 +1,14 @@
 # Add your reusable NixOS modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 # These should be stuff you would like to share with others, not your personal configurations.
-{
   # List your module files here
   # my-module = import ./my-module.nix;
+{
+  legionPatch = { config, pkgs, ... }: {
+    boot.kernelPatches = [
+      {
+        name = "legion-alc287";
+        patch = ./legion-alc287.patch;
+      }
+    ];
+  };
 }
